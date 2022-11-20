3779fb84112eabed96e15975d3866a728a8acc05
superlou <lousimons@gmail.com>
2022-11-20 16:09:16 -0500

    Basic text wrapping

--- app/components/tree-node.js
+++ app/components/tree-node.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class TreeNodeComponent extends Component {
   andPath = 'm -20,20 h 40 v -16 c 0,-30  -40,-30 -40, 0 z';
@@ -6,6 +7,8 @@ export default class TreeNodeComponent extends Component {
     'm -20,20 c 10,-10 30,-10 40,0 v -4 c 0,-20 -16,-36 -20,-36 c -4,0 -20,16 -20,36 z';
   basicElementPath = 'm -25,0 a 25,25 0 1,1 50,0 a 25,25 0 1,1 -50,0';
 
+  @tracked wrappedDesc = this.args.model.desc;
+  
   get pathD() {
     if (this.args.model.nodeType == 'and-gate') {
       return this.andPath;
@@ -14,5 +17,62 @@ export default class TreeNodeComponent extends Component {
     } else if (this.args.model.nodeType == 'basic-event') {
       return this.basicElementPath;
     }
+  }
+  
+  wrapText(el, [desc, maxWidth]) {
+    let padding = 0;
+    let linePadding = 0;
+        
+    // Make hidden copy of this node to draw, measure, and calculate line breaks
+    let testing = el.cloneNode(false);
+    testing.setAttributeNS(null, 'visibility', 'hidden');
+    let testingTSpan = document.createElementNS(null, 'tspan');
+    let testingTextNode = document.createTextNode(desc);
+    testingTSpan.appendChild(testingTextNode);
+    testing.appendChild(testingTSpan);
+    
+    let tester = document.getElementsByTagName('svg')[0].appendChild(testing);
+    
+    let words = desc.split(" ");
+    let prevLine = "";
+    let line = "";
+    let lines = [];
+    let testWidth;
+    
+    for (let n = 0; n < words.length; n++) {
+      line = prevLine + words[n] + " ";
+      testing.textContent = line;
+      testWidth = testing.getBBox().width;
+      
+      if (testWidth > maxWidth) {
+        lines.push(prevLine);
+        prevLine = words[n] + " ";
+      } else {
+        prevLine = line;
+      }
+    }
+    
+    lines.push(prevLine);
+    console.log(lines);
+
+    testing.remove();    
+    el.innerHTML = "";
+    
+    let lineCounter = 0;
+    
+    for (const line of lines) {
+      let tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
+      tspan.textContent = line;
+      tspan.setAttribute('x', 0);
+      tspan.setAttribute('dy', "1.2em");
+      el.appendChild(tspan);
+      lineCounter++;
+    }
+    
+    let bbox = el.getBBox();
+    let g = el.parentElement;
+    let xOffset = -bbox.width/2
+    let yOffset = -bbox.height - 30;
+    g.setAttribute('transform', `translate(${xOffset}, ${yOffset})`);
   }
 }
