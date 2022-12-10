c5acb36bdacd11322e9b6d5d1995cf609af7653c
superlou <lousimons@gmail.com>
2022-12-10 13:13:25 -0500

    Add copy to SVG to fix #14

--- app/controllers/builder.js
+++ app/controllers/builder.js
@@ -45,4 +45,36 @@ export default class BuilderController extends Controller {
       node.save();
     }
   }
+  
+  @action
+  copySvg() {
+    // todo Fix this hackiness to get the SVG. This will break
+    // if we ever have more than one SVG on the page.
+    let svg = document.getElementsByTagName('svg')[0];
+    let parent = svg.parentNode;
+    
+    // Make a temporary svg to prepare for export.
+    // It must be added to the DOM to get styles.
+    svg = svg.cloneNode(true);
+    parent.append(svg);
+    
+    for (let el of svg.getElementsByTagName('path')) {
+      const style = window.getComputedStyle(el);
+      let properties = ['fill', 'stroke', 'stroke-width'];
+      for (const property of properties) {
+        el.style[property] = style[property];
+      }
+    }
+ 
+    for (let el of svg.getElementsByTagName('text')) {
+      const style = window.getComputedStyle(el);
+      let properties = ['text-anchor', 'font-family', 'font-size'];
+      for (const property of properties) {
+        el.style[property] = style[property];
+      }
+    }
+    
+    navigator.clipboard.writeText(svg.outerHTML);
+    parent.removeChild(svg);
+  }
 }
